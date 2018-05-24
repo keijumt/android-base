0d14373e1cb8e3f244d21ade75dfc7c2b60a5966
keijumt <keijumt@gmail.com>
2018-05-24 23:07:03 +0900

    Added project

---
+++ app/src/main/java/keijumt/androidbase/MainActivity.kt
@@ -0,0 +1,19 @@
+package keijumt.androidbase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import keijumt.androidbase.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
