92c5f705812b68c4fa1523cf282dba24acec0c3b
keijumt <matsumoto_k@camobile.com>
2018-05-25 18:17:06 +0900

    Added Timber

--- app/src/main/java/keijumt/androidbase/App.kt
+++ app/src/main/java/keijumt/androidbase/App.kt
@@ -2,10 +2,12 @@ package keijumt.androidbase
 
 import android.app.Activity
 import android.app.Application
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
+import android.util.Log
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -13,6 +15,7 @@ import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.HasSupportFragmentInjector
 import keijumt.androidbase.di.Injectable
 import keijumt.androidbase.di.Injector
+import timber.log.Timber
 
 class App : DaggerApplication() {
 
@@ -24,6 +27,7 @@ class App : DaggerApplication() {
         super.onCreate()
 
         setupActivityLifecycle()
+        setupLog()
     }
 
     private fun handleActivity(activity: Activity) {
@@ -39,6 +43,18 @@ class App : DaggerApplication() {
                         }
                     }
                 }, true)
+    }
+
+    /**
+     * ログ出力セットアップ
+     */
+    private fun setupLog() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Log.i(TAG, "timber.planet normal.")
+        } else {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 
